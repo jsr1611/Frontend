5d1800637c83eeb3518fafc0aa5a1b0eb3ad5851
jimhome_mac <jsr1611@outlook.com>
2024-04-28 06:33:57 +0500

    Added material lib

--- 4. frameworks/angular-app/src/app/app.component.ts
+++ 4. frameworks/angular-app/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, MatToolbarModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
