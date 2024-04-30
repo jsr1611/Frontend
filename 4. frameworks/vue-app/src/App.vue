c6df173a8dca5a5988edd8c90223dd2eddc3a147
jimhome_mac <jsr1611@outlook.com>
2024-04-30 07:01:46 +0500

    Vue-app initial setup with props passed to child component hello-world

---
+++ 4. frameworks/vue-app/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  Hello Vue !
+  <HelloWorld msg="msg from App.vue"> </HelloWorld>
+</template>
+
+<script lang="ts">
+import { Vue, Options } from 'vue-class-component';
+import HelloWorld from "./components/HelloWorld.vue";
+
+@Options({
+ components: {
+ HelloWorld,
+ },
+})
+
+export default class App extends Vue {}
+</script>
+
+<style scoped></style>
---
+++ 4. frameworks/vue-app/src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <p>Hello World</p>
+  <p>msg prop = {{ msg }}</p>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  props: {
+    msg: String
+  }
+})
+
+export default class HelloWorld extends Vue {
+  msg!: string
+}
+</script>
+
+<style scoped>
+</style>
